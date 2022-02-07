420d9028011c13e38b7c80e947c3a8780ca68a4f
Javier Cancela <javiciier@gmail.com>
2022-02-07 12:53:44 +0100

    Create project using Spring Initializer

---
+++ src/main/java/me/unp0wnable/groupeo/GroupeoBackendApplication.java
@@ -0,0 +1,13 @@
+package me.unp0wnable.groupeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GroupeoBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GroupeoBackendApplication.class, args);
+	}
+
+}
